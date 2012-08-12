1b0f2f3911e7ba881fbf4901120d77a5b1ff244e
37point2 <37point2@gmail.com>
2012-08-12 01:21:57 -0400

    Working on class User

--- kloutFF.py
+++ kloutFF.py
@@ -16,8 +16,33 @@ ACCESS_TOKEN_SECRET = api_keys['api']['twitter']['ACCESS_TOKEN_SECRET']
 
 KLOUTAPIKEY = api_keys['api']['klout']['KEY']
 
+#Klout API ref
 
+Kapi = pyklout.Klout(KLOUTAPIKEY)
 
+#Twitter API ref
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+Tapi = tweepy.API(auth)
+
+#set up user class and methods
+
+class User:
+  numUsers = 0
+
+  def __init__(self, name, platform):
+    self.name = name
+    self.platform = platform
+    self.id = Kapi.identity(name, platform)['id']
+    User.numUsers += 1
+
+    print self.name, self.platform
+
+  def influences(self)
+    temp = Kapi.influences(self.id)
+    self.myInfluencersCount = temp['myInfluencersCount']
 
 
 
